a058604c77183a4f5cc23395d323eb4c6d94b3ef
aadityavishnoi <vishnoiaaditya29@gmail.com>
2026-02-14 02:57:25 +0530

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,4 +34,42 @@ const signup = async (req, res) => {
         });
     }
 }
-module.exports = { signup };
+
+const login = async(req, res) => {
+    console.log(req.body);
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if(!user){
+            return res.status(400).json({
+                message: "User Not Found"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({
+                message: "Password Doesn't Match."
+            });
+        }
+
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        );
+
+        res.json({
+            message: "Login Successful!",
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Login Failed"
+        });
+    }
+};
+module.exports = { signup, login };
