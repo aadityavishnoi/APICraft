73a081ffc35a7e7ab11178bdf91c87538f646ca3
aadityavishnoi <vishnoiaaditya29@gmail.com>
2026-03-27 09:05:32 +0530

    commit

---
+++ tmp/test_create.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Collection = require('../models/Collection');
+const User = require('../models/User');
+require('dotenv').config({ path: '../.env' });
+
+mongoose.connect(process.env.MONGO_URI).then(async () => {
+    try {
+        const users = await User.find().limit(2);
+        if(users.length < 2) {
+            console.log('Need 2 users to test');
+            process.exit(0);
+        }
+        const user1 = users[0];
+        const user2 = users[1];
+        
+        await Collection.deleteMany({ collectionName: 'test_col' });
+
+        await Collection.create({ userId: user1._id, collectionName: 'test_col', fields: ['name:String:REQ'] });
+        console.log('User 1 created test_col');
+        
+        await Collection.create({ userId: user2._id, collectionName: 'test_col', fields: ['name:String:REQ'] });
+        console.log('User 2 created test_col');
+        
+        console.log('Successfully created both!');
+    } catch (e) {
+        console.error('Error creating:', e.message);
+    }
+    process.exit(0);
+});
